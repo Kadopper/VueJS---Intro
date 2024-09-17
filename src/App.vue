541b63637723edaeff6b3081abf8aa7c5b67ee39
Kadopper <167879010+Kadopper@users.noreply.github.com>
2024-09-17 10:36:22 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue'; // Import the 'ref' function from Vue to create reactive references
+
+const mouseX = ref(0);
+const mouseY = ref(0);
+// Reactive reference to store the message text
+
+export defualt{
+  data(){
+    return{
+      function showAlert(event) {
+        mouseX.value = client.eventX;
+        mouseY.value = client.eventY;
+      }
+    }
+  }
+}
+</script>
+
+<template>
+  <!-- Display the current message -->
+  <div class="background" @mouseClick="showAlert">
+    <p>{{ mouseX }}</p>
+    <p>{{ mouseY }}</p>
+  </div>
+</template>
