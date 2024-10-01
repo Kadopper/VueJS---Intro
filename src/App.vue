3041c801a84a5a2269d4b4e82a08f3fa073ee1af
Kadopper <christofferditlev@gmail.com>
2024-10-01 18:19:57 +0200

    Finale

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,77 @@
 <script setup>
 import { ref, onMounted } from 'vue';
 
+// changeable variables
 const mouseX = ref(0);
 const mouseY = ref(0);
 const showMenu = ref(false); 
-const radius = 100;
-const numberOfItems = 4
-const angleCircle= 360/numberOfItems
 
+// how far from the mouse the menus should be, the number of item (menus) and calcuates teh angle
+const radius = 100;
+// is changeable yippie!!!1
+const numberOfItems = 6
+const anglesOfCircle = ((2*Math.PI)/numberOfItems)
+
+// when loaded, it add an event listener that open the menu when you click anywhere in the window
+onMounted(() => {
+  window.addEventListener('click', showPosition);
+});
+
+// updates the loaction of the mouse, and the boolean for the menu 
 function showPosition(event) {
   mouseX.value = event.clientX;
   mouseY.value = event.clientY;
   showMenu.value = !showMenu.value;
 }
 
-function showAlert(event){
-  alert("yo")
+
+// display which option was clicked on, every alert uses its index as an argument
+function showAlert(menuIndex) {
+  alert(`This is menu ${menuIndex}`);
 }
-
-
-onMounted(() => {
-  window.addEventListener('click', showPosition);
-});
-
 </script>
 
 <template>
   <div>
+    <div>
+      <!-- display mouse position when window is clicked-->
     <p>X: {{ mouseX }}</p>
     <p>Y: {{ mouseY }}</p>
 
+      <!-- if the showMenu boolean is true, display the menu -->
     <div 
       v-if="showMenu" 
-      @click.stop="showAlert"
-      :style="{ 
-        left: `${mouseX}px`, 
-        top: `${mouseY}px`
-        }" 
-      class="menuBox">
+    >
+      <!-- using v-for, i can render multiple elemetns, wherein i calculate each position in a circle. Furthermore i give it teh ability to show an alert for each menu clicked. -->
+      <div
+        v-for="menuIndex in numberOfItems" 
+        :style="{
+          left: `${mouseX + radius * Math.cos(anglesOfCircle * menuIndex)}px`,
+          top: `${mouseY + radius * Math.sin(anglesOfCircle * menuIndex)}px`,
+          transform: 'translate(-50%, -50%)'
+        }"
+        class="menu"
+        @click="showAlert(menuIndex)"
+      >
+        Menu: {{ menuIndex }}
+      </div>
     </div>
-
+  </div>
   </div>
 </template>
 
 <style scoped>
-.menuBox {
+.menu {
   width: 50px;
   height: 50px;
-  background-color: red;
-  position: absolute;
-  transform: translate(-50%, -50%);
   border-radius: 50%;
+  background-color: blue;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
   cursor: pointer;
+  color: white;
+  font-size: 12px;
 }
 </style>
